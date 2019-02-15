74367c33c8548399de916b1b2340cb39f439ade9
mondara1234 <40348446+mondara1234@users.noreply.github.com>
2019-02-15 19:23:06 +0700

    Update Problems.php

--- src/Problems/Problems.php
+++ src/Problems/Problems.php
@@ -59,7 +59,7 @@
                 <div class="page-breadcrumb">
                     <div class="row">
                         <div class="col-12 d-flex no-block align-items-center">
-                            <h4 class="page-title">ปัญหาที่ผู้ใช้พบ</h4>
+                            <h4 class="page-title">ปัญหาที่พบ</h4>
                         </div>
                     </div>
                 </div>
